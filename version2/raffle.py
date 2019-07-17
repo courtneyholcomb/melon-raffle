311d3e6d3c6714c8f9293f65f0728c197b64807b
Courtney Holcomb <courtneyeholcomb@gmail.com>
2019-07-16 21:46:17 -0700

    Resolved issues in v1 raffle.py, made v2 raffle.py work the same

--- version2/raffle.py
+++ version2/raffle.py
@@ -1,6 +1,24 @@
 """Randomly pick customer and print customer info"""
 
-# Add code starting here
+from random import choice
+from customers import get_customers_from_file
 
-# Hint: remember to import any functions you need from
-# other files or libraries
+
+def pick_winner(customers):
+    """Choose a random winner from list of customers."""
+
+    chosen_customer = choice(customers)
+
+    print(f"Tell {chosen_customer.name} at {chosen_customer.email} "\
+           "that they've won")
+
+
+def run_raffle():
+    """Run the weekly raffle."""
+
+    customers = get_customers_from_file("customers.txt")
+    pick_winner(customers)
+
+
+if __name__ == "__main__":
+    run_raffle()
